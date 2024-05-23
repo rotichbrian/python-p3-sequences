3066af1e41212e6e70d1d8d149b7f94d778ca3b6
Brian Kipkorir <rotichbrian891@gmail.com>
2024-05-23 19:02:38 +0300

    lab completion

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    fibonacci_sequence=[0,1]
+
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    
+    
+    while len(fibonacci_sequence)<length:
+        next_fibonacci = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_fibonacci)
+
+    print(fibonacci_sequence[:length])
+       
     pass
